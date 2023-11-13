ee18b18846258dde5e4a02d95687f602ac24a79f
weslyramalho <weslyramalho@yahoo.com.br>
2023-11-13 16:27:46 -0300

    evoluindo

--- lista_tarefa/app/views.py
+++ lista_tarefa/app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .models import Todo
 
 def todos(request):
-    return render(request, 'todo/index.html')
+    todos =Todo.objects.all()
+    return render(request, 'todo/index.html', {'todos' : todos})
+
+@require_http_methods(['POST'])
+def add(request):
+    todo = None
+    title = request.POST.get('title', '')
+
+    if title:
+        todo = Todo.objects.create(title=title)
+    return render(request, 'todo/partials/todo.html', {'todo': todo})
 
 
--- lista_tarefa/lista_tarefa/urls.py
+++ lista_tarefa/lista_tarefa/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import todos
+from app.views import todos, add
 
 urlpatterns = [
     path('', todos, name='todos'),
+    path('add/', add, name='add'),
     path('admin/', admin.site.urls),
 ]
